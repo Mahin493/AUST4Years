058f0b950554cbfa7013df3033130c25354b9785
MahinOpu <opuaust17@gmail.com>
2022-01-11 15:27:06 +0600

    SD V task 1 .net

---
+++ 3.2/SD V/Assignmentsd/Assignmentsd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignmentsd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var myclass = new Class1();
+           // myclass.Print();
+
+            var mylist1 = new List();
+            mylist1.mylist();
+        }
+    }
+}
