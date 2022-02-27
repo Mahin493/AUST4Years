55bdb58de2e3920fd2f0df5e0caa2ee81202b31b
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-02-27 21:27:42 +0600

    Bfs

---
+++ CSE 2.2/Algorithms Lab/BFS.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+void edge(vector<int>adj[],int u,int v){
+adj[u].push_back(v);
+}
+void bfs(int s,vector<int>adj[],bool visit[]){
+queue<int>q;
+q.push(s);
+visit[s]=true;
+while(!q.empty()){
+int u=q.front();
+cout<<u<<" ";
+q.pop();
+for(int i=0;i<adj[u].size();i++){
+if(!visit[adj[u][i]]){
+q.push(adj[u][i]);
+visit[adj[u][i]]=true;
+}
+}
+}
+}
+int main(){
+
+vector<int>adj[5];
+bool visit[5];
+for(int i=0;i<5;i++){
+visit[i]=false;
+}
+edge(adj,0,2);
+edge(adj,0,1);
+edge(adj,1,3);
+edge(adj,2,0);
+edge(adj,2,3);
+edge(adj,2,4);
+edge(adj,2,5);
+cout<<"BFS traversal is"<<" ";
+bfs(0,adj,visit);
+
+
+
+}
